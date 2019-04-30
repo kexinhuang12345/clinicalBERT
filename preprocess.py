8e48c72bcfee9aa61d7ba3fad7eb7113cafd556c
Kexin <kh2383@nyu.edu>
2019-04-29 23:45:37 -0400

    include an example to train/val/test split

--- preprocess.py
+++ preprocess.py
@@ -97,13 +97,109 @@ def preprocessing(df_less_n):
 
     return df_less_n
         
-preprocessing(df_less_2).to_csv('less_2_days_notes.csv')
-preprocessing(df_less_3).to_csv('less_3_days_notes.csv')
-preprocessing(df_discharge).to_csv('discharge.csv')
-
-### Do K-fold split for each one using sklearn.model_selection import KFold
-
-
-
+df_less_2 = preprocessing(df_less_2)
+df_less_2.to_csv('less_2_days_notes.csv')
+df_less_3 = preprocessing(df_less_3)
+df_less_3.to_csv('less_3_days_notes.csv')
+df_discharge = preprocessing(df_discharge)
+df_discharge.to_csv('discharge.csv')
 
 
+### An example to get the train/test/split with random state:
+### note that we divide on patient admission level and share among experiments, instead of notes level. 
+### This way, since our methods run on the same set of admissions, we can see the
+### progression of readmission scores. 
+
+readmit_ID = pd.Index(df_adm[df_adm.OUTPUT_LABEL == 1].HADM_ID)
+not_readmit_ID = pd.Index(df_adm[df_adm.OUTPUT_LABEL == 0].HADM_ID)
+#subsampling to get the balanced pos/neg numbers of patients for each dataset
+not_readmit_ID_use = not_readmit_ID.sample(n=len(readmit_ID), random_state=1)
+id_val_test_t=readmit_ID.sample(frac=0.2,random_state=1)
+id_val_test_f=not_readmit_ID_use.sample(frac=0.2,random_state=1)
+
+id_train_t = readmit_ID.drop(id_val_test_t.index)
+id_train_f = not_readmit_ID_use.drop(id_val_test_f.index)
+
+id_val_t=id_val_test_t.sample(frac=0.5,random_state=1)
+id_test_t=id_val_test_t.drop(id_val_t.index)
+
+id_val_f=id_val_test_f.sample(frac=0.5,random_state=1)
+id_test_f=id_val_test_f.drop(id_val_f.index)
+
+# test if there is overlap between train and test, should return "array([], dtype=int64)"
+(pd.Index(id_test_t).intersection(pd.Index(id_train_t))).values
+
+id_test = pd.concat([id_test_t, id_test_f])
+test_id_label = pd.DataFrame(data = list(zip(id_test, [1]*len(id_test_t)+[0]*len(id_test_f))), columns = ['id','label'])
+
+id_val = pd.concat([id_val_t, id_val_f])
+val_id_label = pd.DataFrame(data = list(zip(id_val, [1]*len(id_val_t)+[0]*len(id_val_f))), columns = ['id','label'])
+
+id_train = pd.concat([id_train_t, id_train_f])
+train_id_label = pd.DataFrame(data = list(zip(id_train, [1]*len(id_train_t)+[0]*len(id_train_f))), columns = ['id','label'])
+
+#get discharge train/val/test
+
+discharge_train = df_discharge[df_discharge.ID.isin(train_id_label.id)]
+discharge_val = df_discharge[df_discharge.ID.isin(val_id_label.id)]
+discharge_test = df_discharge[df_discharge.ID.isin(test_id_label.id)]
+
+# subsampling for training....since we obtain training on patient admission level so now we have same number of pos/neg readmission
+# but each admission is associated with different length of notes and we train on each chunks of notes, not on the admission, we need
+# to balance the pos/neg chunks on training set. (val and test set are fine) Usually, positive admissions have longer notes, so we need 
+# find some negative chunks of notes from not_readmit_ID that we haven't used yet
+
+df = pd.concat([not_readmit_ID_use, not_readmit_ID])
+df = df.drop_duplicates(keep=False)
+#check to see if there are overlaps
+(pd.Index(df).intersection(pd.Index(not_readmit_ID_use))).values
+
+# for this set of split with random_state=1, we find we need 400 more negative training samples
+not_readmit_ID_more = df.sample(n=400, random_state=1)
+discharge_train_snippets = pd.concat([df_discharge[df_discharge.ID.isin(not_readmit_ID_more)], discharge_train])
+
+#shuffle
+discharge_train_snippets = discharge_train_snippets.sample(frac=1, random_state=1).reset_index(drop=True)
+
+#check if balanced
+discharge_train_snippets.Label.value_counts()
+
+discharge_train_snippets.to_csv('./discharge/train_snippets.csv')
+discharge_val.to_csv('./discharge/val_snippets.csv')
+discharge_test.to_csv('./discharge/test_snippets.csv')
+
+### for Early notes experiment: we only need to find training set for 3 days, then we can test 
+### both 3 days and 2 days. Since we split the data on patient level and experiments share admissions
+### in order to see the progression, the 2 days training dataset is a subset of 3 days training set.
+### So we only train 3 days and we can test/val on both 2 & 3days or any time smaller than 3 days. This means
+### if we train on a dataset with all the notes in n days, we can predict readmissions smaller than n days. 
+
+#for 3 days note, similar to discharge
+
+early_train = df_less_3[df_less_3.ID.isin(train_id_label.id)]
+not_readmit_ID_more = df.sample(n=500, random_state=1)
+early_train_snippets = pd.concat([df_less_3[df_less_3.ID.isin(not_readmit_ID_more)], early_train])
+#shuffle
+early_train_snippets = early_train_snippets.sample(frac=1, random_state=1).reset_index(drop=True)
+early_train_snippets.to_csv('./3days/train_snippets.csv')
+
+early_val = df_less_3[df_less_3.ID.isin(val_id_label.id)]
+early_val.to_csv('./3days/val_snippets.csv')
+
+# we want to test on admissions that are not discharged already. So for less than 3 days of notes experiment,
+# we filter out admissions discharged within 3 days
+actionable_ID_3days = df_adm[df_adm['DURATION'] >= 3].HADM_ID
+test_actionable_id_label = test_id_label[test_id_label.id.isin(actionable_ID_3days)]
+early_test = df_less_3[df_less_3.ID.isin(test_actionable_id_label.id)]
+
+early_test.to_csv('./3days/test_snippets.csv')
+
+#for 2 days notes, we only obtain test set. Since the model parameters are tuned on the val set of 3 days
+
+actionable_ID_2days = df_adm[df_adm['STAY_DAYS'] >= 2].HADM_ID
+
+test_actionable_id_label_2days = test_id_label[test_id_label.id.isin(actionable_ID_2days)]
+
+early_test_2days = df_less_2[df_less_2.ID.isin(test_actionable_id_label_2days.id)]
+
+early_test_2days.to_csv('./2days/test_snippets.csv')
